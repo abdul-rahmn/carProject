207293559c393e882b1a310e3a435686529de833
ibrahim alomar <ibrahimomar@gmail.com>
2022-09-16 10:32:08 +0300

    craete a class and constructor and sets,gets

---
+++ src/Engin.java
@@ -0,0 +1,65 @@
+public class Engin {
+    private int id;
+    private String type;
+    private int maxSpeed;
+    private int numberOfCylenders;
+    private String country;
+    private int carId;
+
+    public Engin(int id, String type, int maxSpeed, int numberOfCylenders, String country, int carId) {
+        this.id = id;
+        this.type = type;
+        this.maxSpeed = maxSpeed;
+        this.numberOfCylenders = numberOfCylenders;
+        this.country = country;
+        this.carId = carId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getNumberOfCylenders() {
+        return numberOfCylenders;
+    }
+
+    public void setNumberOfCylenders(int numberOfCylenders) {
+        this.numberOfCylenders = numberOfCylenders;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+}
