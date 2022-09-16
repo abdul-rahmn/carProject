9e5137aa4e2033aab1863b634cac1a83f48087b0
abdul_Rahman <abdulran60@gmail.com>
2022-09-16 11:43:00 +0300

    create ArrayLists in main

--- src/App.java
+++ src/App.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 public class App {
+    // Create a List for all Objects
+    // Lists Weels
+    static ArrayList<Wheel> wheels1 = new ArrayList<>();
+    static ArrayList<Wheel> wheels2 = new ArrayList<>();
+    static ArrayList<Wheel> wheels3 = new ArrayList<>();
+    // List Engin
+    static ArrayList<Engin> engins = new ArrayList<>();
+    // List Cars
+    static ArrayList<Car> cars = new ArrayList<>();
     public static void main(String[] args) throws Exception {
         
     }
