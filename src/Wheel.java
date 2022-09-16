645b09e4bce54be85b5757c425ca070ed609778e
abdul_Rahman <abdulran60@gmail.com>
2022-09-16 10:21:24 +0300

    create a class Wheel and constructor and sets,gets

---
+++ src/Wheel.java
@@ -0,0 +1,55 @@
+public class Wheel {
+    private int id;
+    private int numberOfScrews;
+    private int thikness;
+    private String type;
+    private int carId;
+
+    public Wheel(int id, int numberOfScrews, int thikness, String type, int carId) {
+        this.id = id;
+        this.numberOfScrews = numberOfScrews;
+        this.thikness = thikness;
+        this.type = type;
+        this.carId = carId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumberOfScrews() {
+        return numberOfScrews;
+    }
+
+    public void setNumberOfScrews(int numberOfScrews) {
+        this.numberOfScrews = numberOfScrews;
+    }
+
+    public int getThikness() {
+        return thikness;
+    }
+
+    public void setThikness(int thikness) {
+        this.thikness = thikness;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+}
